ed68e693365da456a8af95892890facd1ffa144e
iamnitinmisra <nitin.misra84@gmail.com>
2020-07-18 22:40:49 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
+
+// Server
+const app = express();
+app.use(express.json())
+
+// Session
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 60 * 24 * 7},
+    secret: SESSION_SECRET
+}))
+
+// Endpoints
+
+// Database & Server Connection
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+}).then(db => {
+    app.set("db", db)
+    console.log("Database connection established")
+    app.listen(SERVER_PORT, ()=>console.log(`Server is listening on ${SERVER_PORT}`))
+})
